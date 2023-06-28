ce392d4199b17f199c94b8a3699b8d3b60e3aedc
victor1736 <victoralfonsofernandezhoyos@gmail.com>
2023-06-28 11:46:18 -0500

    correcion
    correcion

--- crops/homecrops/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HomecropsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'homecrops'
--- crops/homecrops/urls.py
+++
@@ -1,6 +0,0 @@
-from homecrops.views import index
-from django.urls import path
-
-urlpatterns = [
-    path('', index, name='index'),
-]
